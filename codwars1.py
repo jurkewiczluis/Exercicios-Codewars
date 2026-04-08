00d9b9bddc4a9153ee91204da17294889f8e1448
Luis Felipe Jurkewicz <luisfelipejurkewicz@gmail.com>
2026-04-08 00:50:23 -0300

    Pirmeiro Exercicio

---
+++ codwars1.py
@@ -0,0 +1,10 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+
+    return "Odd"
+
+print(even_or_odd(10))
+print(even_or_odd(5))
+print(even_or_odd(0))
+print(even_or_odd(-4))
